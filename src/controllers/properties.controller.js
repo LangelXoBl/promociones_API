d08bb509d84b54ebb3c63af77bbc78ab5d3f5888
lange <langelxobl@gmail.com>
2022-10-03 15:34:11 -0500

    properties

--- src/controllers/properties.controller.js
+++ src/controllers/properties.controller.js
@@ -1,6 +1,14 @@
 import propertiesModel from "../models/properties.model.js";
+import promocionesModel from "../models/promociones.model.js";
 
 export const properties = async (req, res) => {
-  const all = await propertiesModel.find({});
-  res.json(all);
+  try {
+    const all = await propertiesModel.find({_id:"5bac113cfb02bf3577c8f955"});
+    const promo = await promocionesModel.find({desarrollo:{code:"the-boat"}});
+    res.json({all, promo});
+  } catch (Error) {
+    res
+      .statu(500)
+      .json({ message: Error.message || "Error al devolver properties" });
+  }
 };
