bbd5f09f9ff6fa51307c23874d40c3a747558b72
Luis Xool <langelxobl@gmail.com>
2023-01-24 11:19:34 -0500

    Data base on attlas

--- src/DB.js
+++ src/DB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 (async () => {
   try {
-    const db = await mongoose.connect("mongodb://localhost:27017/prue");
+    const db = await mongoose.connect("mongodb+srv://xool:202000098@cluster0.rr55yis.mongodb.net/test?retryWrites=true&w=majority");
     console.log("Conected with dabatabe", db.connection.name);
   } catch (e) {
     console.error(e);
